dca1180ded6917e3d3937a2ce8c31991030199e3
Anthony Coutinho da Silva <75925172+thonycsdev@users.noreply.github.com>
2023-11-13 19:16:25 +0000

    .env e configuracoes de database.js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response){
-  return response.status(200).json({fruta: "Banana"});
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 AS sum;");
+  console.log(result.rows);
+  return response.status(200).json({ fruta: "Banana" });
 }
 
 export default status;
