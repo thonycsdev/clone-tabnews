77ebbcb2f7d8eada3e60793b4612bc5543b013cf
Anthony Silva <thonycout@gmail.com>
2023-12-03 14:17:56 -0300

    exercicios propostos

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,21 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS sum;");
-  console.log(result.rows);
-  return response.status(200).json({ fruta: "Banana" });
+  const updatedAt = new Date().toISOString();
+  const result = await database.query("Select VERSION()");
+  const maxConnections = await database.query("SHOW max_connections");
+  const activeConnections = await database.query(
+    "SELECT count(distinct(numbackends)) FROM pg_stat_database;"
+  );
+  const activeConnectionsNumber = activeConnections.rows[0].count;
+  const maxConnectionsNumber = maxConnections.rows[0].max_connections;
+  const database_version = result.rows[0].version.split(" ")[1];
+  return response.status(200).json({
+    updated_at: updatedAt,
+    database_version,
+    max_connections: maxConnectionsNumber,
+    active_connections: activeConnectionsNumber,
+  });
 }
 
 export default status;
--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
 test("/api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
   expect(response.status).toBe(200);
-})
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedDate = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedDate);
+  expect(responseBody.database_version).toBeDefined();
+  expect(responseBody.database_version).toBe("16.0");
+  expect(responseBody.max_connections).toBeDefined();
+  expect(responseBody.max_connections).toBe("100");
+  expect(responseBody.active_connections).toBeDefined();
+  expect(parseInt(responseBody.active_connections)).toBeGreaterThan(0);
+  expect(parseInt(responseBody.active_connections)).toBeLessThanOrEqual(
+    parseInt(responseBody.max_connections)
+  );
+
+  console.log(responseBody);
+});
