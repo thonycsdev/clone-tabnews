1ece3159c65d2796baa108e838c22236c75bd68d
Anthony Coutinho <anthony.eldyn@gmail.com>
2023-12-18 22:36:57 -0300

    terminando endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,19 +2,28 @@ import database from "infra/database";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
-  const result = await database.query("Select VERSION()");
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const database_version = databaseVersionResult.rows[0].server_version;
+
   const maxConnections = await database.query("SHOW max_connections");
-  const activeConnections = await database.query(
-    "SELECT count(distinct(numbackends)) FROM pg_stat_database;"
-  );
-  const activeConnectionsNumber = activeConnections.rows[0].count;
   const maxConnectionsNumber = maxConnections.rows[0].max_connections;
-  const database_version = result.rows[0].version.split(" ")[1];
+  const databaseName = process.env.POSTGRES_DB;
+  const activeConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname=$1;",
+    values: [databaseName],
+  });
+  const activeConnectionsValue = activeConnections.rows[0].count;
+
   return response.status(200).json({
     updated_at: updatedAt,
-    database_version,
-    max_connections: maxConnectionsNumber,
-    active_connections: activeConnectionsNumber,
+    dependencies: {
+      database: {
+        version: database_version,
+        max_connections: maxConnectionsNumber,
+        opened_connections: activeConnectionsValue,
+      },
+      webserver: {},
+    },
   });
 }
 
