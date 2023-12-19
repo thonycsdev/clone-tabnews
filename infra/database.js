4138b42d66a35ffcbb69d68a3aad114ca0a8dc95
Anthony Coutinho <anthony.eldyn@gmail.com>
2023-12-18 22:36:27 -0300

    mais robusto para lidar com erros

--- infra/database.js
+++ infra/database.js
@@ -8,10 +8,15 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
   });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.end();
+  }
 }
 export default {
   query: query,
