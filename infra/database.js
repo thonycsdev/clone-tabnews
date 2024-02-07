1ee3ab48a92a987a3c5a4b8b6814941557145be6
thonycsdev <thonycout@gmail.com>
2024-02-07 16:14:40 -0300

    Changing `database.js` and `client.connect()` to be more robust against errors

--- infra/database.js
+++ infra/database.js
@@ -8,12 +8,21 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
   });
+  console.log(
+    "Database Connection Data",
+    process.env.POSTGRES_HOST,
+    process.env.POSTGRES_PORT,
+    process.env.POSTGRES_USER,
+    process.env.POSTGRES_PASSWORD,
+    process.env.POSTGRES_DB
+  );
   try {
     await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.log(error);
+    throw error;
   } finally {
     await client.end();
   }
