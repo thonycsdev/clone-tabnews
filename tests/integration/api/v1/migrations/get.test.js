06d2feaef54d07d34ca0d40ee3e1e555258ab78f
Anthony Silva <anthony.eldyn@gmail.com>
2024-10-21 16:11:05 -0300

    makes > clone-tabnews@1.0.0 test > npm run services:up && npm run wait-for-postgres && concurrently -n next,jest --hide next -k -s command-jest 'next dev' 'jest --runInBand'
    > clone-tabnews@1.0.0 services:up
    > docker compose -f infra/compose.yaml up -d
    
    > clone-tabnews@1.0.0 wait-for-postgres
    > node ./infra/scripts/wait-for-postgres.js
    
    --> Waiting for postgres to accept connections.
    ...............................
    
    --> Yes! Everything is working fine here ':)'
    
    [jest] PASS tests/integration/api/v1/status/get.test.js
    [jest] PASS tests/integration/api/v1/migrations/post.test.js
    [jest] PASS tests/integration/api/v1/migrations/get.test.js
    [jest]
    [jest] Test Suites: 3 passed, 3 total
    [jest] Tests:       7 passed, 7 total
    [jest] Snapshots:   0 total
    [jest] Time:        1.156 s, estimated 2 s
    [jest] Ran all test suites.
    [jest] jest --runInBand exited with code 0
    --> Sending SIGTERM to other processes.. more robust with zsh: command not found: async-retries and zsh: command not found: orchestrator.js

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,9 +1,10 @@
 import database from "infra/database";
+import orchestrator from "tests/orchestrator";
 
-beforeAll(cleanDatabase);
-async function cleanDatabase() {
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
-}
+});
 test("/api/v1/migrations/ GET should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -4,10 +4,10 @@ async function waitForAllServices() {
 }
 
 async function waitForWebServer() {
-  return retry(fetchStatusPage);
+  return retry(fetchStatusPage, { retries: 100, maxTimeout: 1000 });
 }
 
-async function fetchStatusPage() {
+async function fetchStatusPage(bail, tryNumber) {
   const response = await fetch("http://localhost:3000/api/v1/status");
   const responseBody = await response.json();
 }
