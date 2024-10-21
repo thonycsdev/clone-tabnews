216d23b50fe5e80ae1236e4cbd50be9d0546b33b
Anthony Silva <anthony.eldyn@gmail.com>
2024-10-21 15:46:13 -0300

    aula_dia_28

---
+++ tests/orchestrator.js
@@ -0,0 +1,15 @@
+import retry from "async-retry";
+async function waitForAllServices() {
+  await waitForWebServer();
+}
+
+async function waitForWebServer() {
+  return retry(fetchStatusPage);
+}
+
+async function fetchStatusPage() {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+}
+
+export default { waitForAllServices };
