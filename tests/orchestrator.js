c106d875c5e9f18d9ecf8291d33a804b9d4b530b
Anthony Silva <anthony.eldyn@gmail.com>
2024-10-23 14:05:43 -0300

    melhorando o log de erros no zsh: command not found: orchestrator.js para verificar o status da messagem apos o fetch

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -4,12 +4,22 @@ async function waitForAllServices() {
 }
 
 async function waitForWebServer() {
-  return retry(fetchStatusPage, { retries: 100, maxTimeout: 1000 });
+  return retry(fetchStatusPage, {
+    retries: 100,
+    maxTimeout: 1000,
+    onRetry: retryErroLogMessage,
+  });
 }
 
-async function fetchStatusPage(bail, tryNumber) {
+async function fetchStatusPage() {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  const responseBody = await response.json();
+  if (!response.ok) {
+    throw new Error(`Fail to fetch status ${response.status}`);
+  }
+}
+
+async function retryErroLogMessage(err, attempt) {
+  console.error(`Error on attempt: ${attempt}. ${err.message}`);
 }
 
 export default { waitForAllServices };
