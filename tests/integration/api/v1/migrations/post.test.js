f24cd1e393514cb78538a33d144383e9d7e91963
Anthony Coutinho da Silva <75925172+thonycsdev@users.noreply.github.com>
2024-12-05 13:51:03 +0000

    refactor: refact `post.test.ts`

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -5,20 +5,33 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("/api/v1/migrations/ POST should return 200", async () => {
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous User", () => {
+    describe("Running pending migrations", () => {
+      test("For the FIRST time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response.status).toBe(201);
+        const responseBody1 = await response.json();
+        expect(Array.isArray(responseBody1)).toBeTruthy();
+        expect(responseBody1.length).toBeGreaterThanOrEqual(1);
+      });
+      test("For the SECOND time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response.status).toBe(200);
+        const responseBody = await response.json();
+        expect(Array.isArray(responseBody)).toBeTruthy();
+        expect(responseBody.length).toBe(0);
+      });
+    });
   });
-  expect(response1.status).toBe(201);
-  const responseBody1 = await response1.json();
-  expect(Array.isArray(responseBody1)).toBeTruthy();
-  expect(responseBody1.length).toBeGreaterThanOrEqual(1);
-
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(response2.status).toBe(200);
-  const responseBody = await response2.json();
-  expect(Array.isArray(responseBody)).toBeTruthy();
-  expect(responseBody.length).toBe(0);
 });
