1a4dce9015d9d09cb1ee5bea31be1dc2fd84a684
Anthony Coutinho da Silva <75925172+thonycsdev@users.noreply.github.com>
2025-04-01 23:21:51 +0000

    refactor: create `migrator.js` to handle migrations operations

---
+++ models/migrator.js
@@ -0,0 +1,29 @@
+import database from "infra/database";
+import path from "node:path";
+import NodePGMigrate from "node-pg-migrate";
+
+async function runPendingMigrations({ isDryRun } = true) {
+  const nodePGMigrateDefaultConfiguration =
+    await getNodePGMigrationConfiguration();
+  const migratedMigrations = await NodePGMigrate({
+    ...nodePGMigrateDefaultConfiguration,
+    dryRun: isDryRun,
+  });
+  return migratedMigrations;
+}
+
+async function getNodePGMigrationConfiguration() {
+  let dbClient;
+  dbClient = await database.getNewClient();
+  return {
+    dbClient: dbClient,
+    dir: path.resolve("infra", "migrations"),
+    dryRun: false,
+    direction: "up",
+    verbose: "true",
+    migrationsTable: "pgMigrations",
+  };
+}
+
+const migrator = { runPendingMigrations };
+export default migrator;
