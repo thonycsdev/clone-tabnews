eaa00e5d6cb89ad4286980dd9afe83c9b8606672
Anthony Coutinho da Silva <75925172+thonycsdev@users.noreply.github.com>
2025-04-05 16:24:39 +0000

    perf: rebuilding codespaces

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -1,5 +1,5 @@
-import database from "infra/database";
 import orchestrator from "tests/orchestrator";
+import { version as uuidVersion } from "uuid";
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
   await orchestrator.resetDatabase();
@@ -9,16 +9,24 @@ beforeAll(async () => {
 describe("POST /api/v1/users", () => {
   describe("Anonymous User", () => {
     test("With unique and valid data", async () => {
-      await database.query({
-        text: "INSERT INTO users (username, email) values ($1, $2);",
-        values: ["thonycsdev", "thonycsdev@email.com"],
-      });
-      const resultFromUsersTable = await database.query("SELECT * FROM users;");
-      console.log(resultFromUsersTable.rows);
       const response = await fetch("http://localhost:3000/api/v1/users", {
         method: "POST",
       });
+
       expect(response.status).toBe(201);
+      const responseBody = await response.json();
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNan();
+      expect(Date.parse(responseBody.created_at)).not.toBeNan();
+
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        username: "thonycsdev",
+        email: "thonycsdev@email.com",
+        password: "12345678",
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
     });
   });
 });
