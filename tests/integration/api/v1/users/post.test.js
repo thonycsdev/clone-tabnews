f214cd7c66a6b4ca485603c9bea8fc78177f7153
Anthony <thonycout@gmail.com>
2025-05-17 13:53:29 -0300

    feat: add user creation endpoint

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -23,7 +23,6 @@ describe("POST /api/v1/users", () => {
       expect(uuidVersion(responseBody.id)).toBe(4);
       expect(Date.parse(responseBody.created_at)).not.toBeNaN();
       expect(Date.parse(responseBody.created_at)).not.toBeNaN();
-
     });
 
     test("With duplicate email", async () => {
@@ -49,6 +48,48 @@ describe("POST /api/v1/users", () => {
 
       expect(response2.status).toBe(400);
 
+      const responseBody2 = await response2.json();
+      expect(responseBody2.name).toBe("ValidationError");
+      expect(responseBody2.message).toBe(
+        "O email utilizado ja foi cadastrado.",
+      );
+      expect(responseBody2.action).toBe(
+        "Utilize outro email que nao tenha sido cadastrado anteriormente.",
+      );
+      expect(responseBody2.status_code).toBe(400);
+    });
+    test("With duplicate username", async () => {
+      const response1 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        body: JSON.stringify({
+          username: "usernameDuplicated",
+          password: "123456",
+          email: "userEmail1@email.com",
+        }),
+      });
+
+      expect(response1.status).toBe(201);
+
+      const response2 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        body: JSON.stringify({
+          username: "usERnameDuplicateD",
+          password: "123456",
+          email: "userEmail2@email.com",
+        }),
+      });
+
+      expect(response2.status).toBe(400);
+
+      const responseBody2 = await response2.json();
+      expect(responseBody2.name).toBe("ValidationError");
+      expect(responseBody2.message).toBe(
+        "O username utilizado ja foi cadastrado.",
+      );
+      expect(responseBody2.action).toBe(
+        "Utilize outro username que nao tenha sido cadastrado anteriormente.",
+      );
+      expect(responseBody2.status_code).toBe(400);
     });
   });
 });
