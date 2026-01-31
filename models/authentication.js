7b4788d1edcda9816d623ca01d3e871975772090
Anthony Coutinho da Silva <75925172+thonycsdev@users.noreply.github.com>
2026-01-31 20:02:49 +0000

    refactor: entendendo niveis de refatoracao

--- models/authentication.js
+++ models/authentication.js
@@ -1,18 +1,48 @@
 import user from "models/user";
 import password from "models/password";
-import {UnauthorizedError} from "infra/errors"
+import { NotFoundError, UnauthorizedError } from "infra/errors";
 
-async function getAuthenticatedUser(data){
-    const storedUser = await user.findOneByEmail(data.email);
-    const correctPasswordMatch = await password.compare(data.password, storedUser.password);
-    if(!correctPasswordMatch){
-       throw new UnauthorizedError({
-      message: "Senha não confere.",
-      action: "Verifique se o dado está correto."
-    })
+async function getAuthenticatedUser(data) {
+  try {
+    const storedUser = await findUserByEmail(data.email);
+    await validatePassword(data.password, storedUser.password);
+    return storedUser;
+  } catch (error) {
+    if (error instanceof UnauthorizedError) {
+      throw new UnauthorizedError({
+        message: "Dados de autenticação não conferem",
+        action: "Verifique se os dados informados estão corretos",
+      });
     }
-    return storedUser
+  }
+  async function findUserByEmail(userEmail) {
+    try {
+      const storedUser = await user.findOneByEmail(userEmail);
+      return storedUser;
+    } catch (error) {
+      //O erro que findOne gera, precisa ser oculto para evitar vazamento de informacoes
+      if (error instanceof NotFoundError) {
+        throw new UnauthorizedError({
+          message: "Senha não confere.",
+          action: "Verifique se o dado está correto.",
+        });
+      }
+      throw error;
+    }
+  }
+  async function validatePassword(providedPassword, storedPassword) {
+    const correctPasswordMatch = await password.compare(
+      providedPassword,
+      storedPassword,
+    );
+    if (!correctPasswordMatch) {
+      throw new UnauthorizedError({
+        message: "Senha não confere.",
+        action: "Verifique se o dado está correto.",
+      });
+    }
+  }
 }
 
-const authentication = {getAuthenticatedUser}
+const authentication = { getAuthenticatedUser };
 export default authentication;
--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -1,7 +1,6 @@
 import controller from "infra/controller";
 import { createRouter } from "next-connect";
 import authentication from "models/authentication";
-import { UnauthorizedError } from "infra/errors";
 
 const router = createRouter();
 
@@ -11,14 +10,6 @@ export default router.handler(controller.errorHandlers);
 
 async function postHandler(request, response) {
   const data = request.body;
-  try {
-    const authenticatedUser = await authentication.getAuthenticatedUser(data);
-
-    return response.status(200).json(authenticatedUser);
-  } catch (error) {
-    throw new UnauthorizedError({
-      message: "Dados de autenticação não conferem",
-      action: "Verifique se os dados informados estão corretos",
-    });
-  }
+  const authenticatedUser = await authentication.getAuthenticatedUser(data);
+  return response.status(200).json(authenticatedUser);
 }
